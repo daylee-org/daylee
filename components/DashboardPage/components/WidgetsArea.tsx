08ecfc23d9de81380f2bc7a6f965cf1e6a440653
Chi-Luc <chilucdiep@hotmail.com>
2022-02-07 19:44:38 -0500

    Widgets grid

--- components/DashboardPage/components/WidgetsArea.tsx
+++ components/DashboardPage/components/WidgetsArea.tsx
@@ -1,4 +1,5 @@
-import { Stack } from 'components';
+import { Stack, Typography } from 'components';
+import styles from './WidgetsArea.module.scss';
 
 export function WidgetsArea() {
   return (
@@ -6,18 +7,44 @@ export function WidgetsArea() {
       height="100%"
       width="100%"
       scroll
-      // spacing="normal"
       vertical
+      center
       py="10px"
       id="widgets"
     >
-      <Stack
-        height="100%"
-        width="100%"
-        background="widgetBackgroundColor"
-      >
-        Widgets
-      </Stack>
+      <div className={styles.Grid}>
+        <GridItem
+          height="300px"
+          title="Habit Tracker"
+          id="habit-tracker"
+        />
+        <GridItem
+          height="300px"
+          title="Weather App"
+          id="weather"
+        />
+      </div>
     </Stack>
+  );
+}
+
+interface GridItemProps {
+  title: string;
+  height: string;
+  id: string;
+}
+
+function GridItem({ title, height, id }: GridItemProps) {
+  return (
+    <div className={styles.GridItem} id={styles[id]}>
+      <Stack
+        height={height}
+        background="widgetBackgroundColor"
+        py="normal"
+        px="tight"
+      >
+        <Typography type="header5">{title}</Typography>
+      </Stack>
+    </div>
   );
 }
