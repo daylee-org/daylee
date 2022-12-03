4ee99d8526ee6af17607f18d300276af76670701
chiludiep <chiludiep@hotmail.com>
2022-12-03 17:28:29 -0500

    Add Habit

--- components/DashboardPage/components/HabitTracker/HabitsList.tsx
+++ components/DashboardPage/components/HabitTracker/HabitsList.tsx
@@ -3,30 +3,38 @@ import { Button, Stack } from 'components';
 import { Habit } from './Habit';
 
 interface Habit {
-  id: number;
   label: string;
 }
 
 export function HabitsList() {
   const [habitsList, setHabitsList] = useState<Habit[]>([
     {
-      id: 1,
       label: 'Habit 1',
     },
   ]);
 
   return (
     <Stack vertical>
-      <Stack>
+      <Stack vertical spacing="tight">
         {habitsList.map((habit) => (
-          <Habit key={habit.id} label={habit.label} />
+          <Habit key={habit.label} label={habit.label} />
         ))}
       </Stack>
       <Button
         fontSize="medium"
         label="New"
         variant="add"
+        onClick={handleAddHabit}
       ></Button>
     </Stack>
   );
+
+  function handleAddHabit() {
+    setHabitsList([
+      ...habitsList,
+      {
+        label: 'New Habit',
+      },
+    ]);
+  }
 }
