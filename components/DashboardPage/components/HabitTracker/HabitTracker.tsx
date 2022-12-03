be0743bc9f71a59d917c04c64a6327f18a4da351
chiludiep <chiludiep@hotmail.com>
2022-12-03 16:55:27 -0500

    Date heading

--- components/DashboardPage/components/HabitTracker/HabitTracker.tsx
+++ components/DashboardPage/components/HabitTracker/HabitTracker.tsx
@@ -1,37 +1,57 @@
-import { useState } from 'react';
+import { Stack, Typography } from 'components';
 
 export function HabitTracker() {
   let orderedWeekdays: string[] = [];
 
   const weekDays = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
+    'Sun',
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat',
   ];
 
-  const weekdayNumber = new Date().getDay();
-  console.log(weekdayNumber);
-  let currentWeekDayNumber = weekdayNumber;
-
-  for (let i = 0; i < weekDays.length; i++) {
-    let currentweekDay = weekDays[currentWeekDayNumber];
-
-    orderedWeekdays = [...orderedWeekdays, currentweekDay];
-
-    currentWeekDayNumber < 6
-      ? currentWeekDayNumber++
-      : (currentWeekDayNumber = 0);
-  }
+  setOrderedWeekDays();
 
   return (
-    <div>
-      {orderedWeekdays.map((weekDay) => (
-        <div key={weekDay}>{weekDay}</div>
-      ))}
-    </div>
+    <>
+      <Stack spacing="tight">
+        <Stack width="25%">ok</Stack>
+        <Stack width="75%">
+          <Stack
+            width="100%"
+            spread
+            background="dangerTextColor"
+          >
+            {orderedWeekdays.map((weekDay) => (
+              <Typography key={weekDay}>
+                {weekDay}
+              </Typography>
+            ))}
+          </Stack>
+        </Stack>
+      </Stack>
+    </>
   );
+
+  function setOrderedWeekDays() {
+    const weekdayNumber = new Date().getDay();
+    let currentWeekDayNumber = weekdayNumber;
+
+    for (let i = 0; i < weekDays.length; i++) {
+      console.log(orderedWeekdays);
+      let currentweekDay = weekDays[currentWeekDayNumber];
+
+      orderedWeekdays = [
+        ...orderedWeekdays,
+        currentweekDay,
+      ];
+
+      currentWeekDayNumber < 6
+        ? currentWeekDayNumber++
+        : (currentWeekDayNumber = 0);
+    }
+  }
 }
