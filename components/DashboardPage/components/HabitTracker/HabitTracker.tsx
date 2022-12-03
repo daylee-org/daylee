dc53618a4dd0fc49171ba4a5cad4ed19dede4b4e
chiludiep <chiludiep@hotmail.com>
2022-12-03 15:50:31 -0500

    Ordered weekdays

--- components/DashboardPage/components/HabitTracker/HabitTracker.tsx
+++ components/DashboardPage/components/HabitTracker/HabitTracker.tsx
@@ -1,3 +1,37 @@
+import { useState } from 'react';
+
 export function HabitTracker() {
-  return <div>ok</div>;
+  let orderedWeekdays: string[] = [];
+
+  const weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  const weekdayNumber = new Date().getDay();
+  console.log(weekdayNumber);
+  let currentWeekDayNumber = weekdayNumber;
+
+  for (let i = 0; i < weekDays.length; i++) {
+    let currentweekDay = weekDays[currentWeekDayNumber];
+
+    orderedWeekdays = [...orderedWeekdays, currentweekDay];
+
+    currentWeekDayNumber < 6
+      ? currentWeekDayNumber++
+      : (currentWeekDayNumber = 0);
+  }
+
+  return (
+    <div>
+      {orderedWeekdays.map((weekDay) => (
+        <div key={weekDay}>{weekDay}</div>
+      ))}
+    </div>
+  );
 }
