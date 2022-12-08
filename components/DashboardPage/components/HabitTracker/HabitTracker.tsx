3fbe0872f402bcf184d43599a775e0dff1b28721
chiludiep <chiludiep@hotmail.com>
2022-12-08 18:06:49 -0500

    Habit scroll

--- components/DashboardPage/components/HabitTracker/HabitTracker.tsx
+++ components/DashboardPage/components/HabitTracker/HabitTracker.tsx
@@ -1,11 +1,12 @@
+import { Stack } from 'components';
 import { HabitsList } from './HabitsList';
 import { Header } from './Header';
 
 export function HabitTracker() {
   return (
-    <>
+    <Stack vertical scroll>
       <Header />
       <HabitsList />
-    </>
+    </Stack>
   );
 }
